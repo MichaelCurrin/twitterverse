f8b0431d92abfe1df206d68186af950802e59a60
Michael Currin <michael@devdell09.piccing.office>
2017-07-16 20:24:37 +0200

    * ISSUE-8: Get trends in two cities.

--- app/app.py
+++ app/app.py
@@ -56,20 +56,11 @@ def searchTowns(searchStr='', startswith=False):
     return res
 
 
-def _test():
-    global api
+### Tempory tests ###
 
-    cityWoeid = conf.getint('tests', 'cityWoeid')
-    # Ignore 'created_at' and 'as_of' and just use 'trends'. There is a list of one item so we can have to take the item.
-    if False:
-        cityTrends = api.trends_place(cityWoeid)[0]['trends']
-    ##print api.trends_place(cityWoeid)[0]['created_at']
-    ## 2017-07-14T22:03:14Z
-    ## GMT+0000
-    
-    '''
+def _testSearchTowns(searchStr='ca'):
     print 'TOWNS SEARCH'
-    searchStr = 'ca'
+    print searchStr
     res = searchTowns(searchStr)
     print
     print str(res)
@@ -78,9 +69,22 @@ def _test():
         print u'{:10d} - {}'.format(town.woeid, town.name)
     print
     print '-------'
+    '''
+    # Sample
+    23424800 - Dominican Republic
+     *      76456 - Santo Domingo
 
+    23424942 - South Africa
+     *    1580913 - Durban
+     *    1582504 - Johannesburg
+     *    1586614 - Port Elizabeth
+     *    1586638 - Pretoria
+     *    1591691 - Cape Town
+    '''
+
+def _testSearchCountry(searchStr='ica'):
     print 'COUNTRY SEARCH'
-    searchStr = 'ica'
+    print searchStr
     res = searchCountry(searchStr)
     print
     print str(res)
@@ -90,19 +94,67 @@ def _test():
         for town in country.hasTowns:
             print u' * {:10d} - {}'.format(town.woeid, town.name)
         print
-    
+    '''   
+    # Sample   
+          8775 - Calgary
+         15127 - Cardiff
+         30079 - Newcastle
+        110978 - Acapulco
+        111579 - Aguascalientes
+        118466 - Ecatepec de Morelos
+        133475 - Mexicali
+        134395 - Naucalpan de Juárez
+        149769 - Toluca
+        368149 - Cali
+        395269 - Caracas
+        395270 - Maracaibo
+        395271 - Maracay
+        455828 - Campinas
+       1100968 - Canberra
+       1167715 - Calocan
+       1199002 - Cagayan de Oro
+       1252351 - Can Tho
+       1521894 - Cairo
+       1591691 - Cape Town
+       1939897 - Mecca
+       2268284 - Muscat
+       2379574 - Chicago
+    '''
+
+
+def _testTweepyTrends():
+    """
+    This is used for testing only and can be replaced by other functions which
+    are in use.
+    """
+    global api
+    # Read in configured city ID.
+    cityWoeid = conf.getint('tests', 'cityWoeid')
+
+    # Ignore 'created_at' and 'as_of' and just use 'trends'. There is a list of one item so we have to take first and only the item.
+
+    cityTrends = api.trends_place(cityWoeid)[0]['trends']
+    ##print api.trends_place(cityWoeid)[0]['created_at']
+    ## 2017-07-14T22:03:14Z
+    ## note this is GMT+0000
+
     print 'GET TRENDS FOR LOCATION FROM TWEEPY'
     for x in cityTrends:
         topic = x['name']
         volume = x['tweet_volume']
         t = db.Trend(topic=topic, volume=volume).setPlace(cityWoeid)
         print u'Created - Trend: {0} | {1} | {2}.'.format(cityWoeid, topic, volume)
-    '''
+
+
+def _testHighestWithDate():
+    global api
+
+    cityWoeid = conf.getint('tests', 'cityWoeid')
 
     print 'GET HIGHEST TRENDS FOR LOCATION, FOR DATE RANGE'
     # End date will start at midnight so have to add a day to include whole day. And we use < to exclude start of the day.
     endDate = datetime.date.today() + datetime.timedelta(days=1)
-    startDate = endDate - datetime.timedelta(days=1)
+    startDate = endDate - datetime.timedelta(days=7)
     print startDate
     print endDate
     # use timezone aware column for timestamp?
@@ -110,17 +162,17 @@ def _test():
 
     # use datetime.datetime for past 24 hours instead of today?
 
-    # for `byWoeid`, have to use Place name, not Town table, otherwise get error for ambiguous id column.
-    city = db.Place.byWoeid(cityWoeid)
-    assert city is not None, 'Expected city to be returned for {}.'.format(cityWoeid)
-    cityID = city.id
-
-    # Or use `select`. Note that `selectBy` gave same error as other method above on Town.
-    # Though in raw SQL this ends up being on Place anyway so no benefit is achieved.
-    # cityRes = db.Town.select(db.Town.q.woeid == cityWoeid)
-    # city = cityRes.getOne() if cityRes else None
-    # assert city is not None, 'Expected city to be returned for {}.'.format(cityWoeid)
-    # cityID = city.id
+    if True:
+        # Using Town causes an error on ambiguous sort id. And adding order by here doesn't work.
+        city = db.Place.byWoeid(cityWoeid)
+        assert city is not None, 'Expected city to be returned for {}.'.format(cityWoeid)
+        cityID = city.id
+    else:
+        # Alternative.
+        cityRes = db.Town.select(db.Town.q.woeid == cityWoeid)
+        city = cityRes.getOne() if cityRes else None
+        assert city is not None, 'Expected city to be returned for {}.'.format(cityWoeid)
+        cityID = city.id
 
     res = db.Trend.select(
         builder.AND(db.Trend.q.placeID == cityID,
@@ -128,10 +180,133 @@ def _test():
                     db.Trend.q.timestamp < endDate,
                     )
         ).orderBy('volume desc').limit(5)
+
     print res
     for x in res:
         print x
 
 
+def insertTrendsForWoeid(woeid, delete=False):
+    """
+    Receives a WOEID value for a Place, gets up to 50 trend records for the
+    Place and stores each of the values in the Trend table.
+
+    From trend API request response, ignore the location which we know and 
+    times which we don't need if we just use current time.
+
+    @param woeid: Integer WOEID for a Place.
+    @parma insert: Boolean, default False. If set to True, delete item after
+        it is inserted into db. This is useful for testing.
+    """
+    global api
+    response = api.trends_place(woeid)[0]
+    trends = response['trends']
+
+    for x in trends:
+        topic = x['name']
+        volume = x['tweet_volume']
+        t = db.Trend(topic=topic, volume=volume).setPlace(woeid)
+        print u'Added trend: {0:4d} | {1:25} - {2:10,d} | {3:10} - {4}.'\
+                .format(t.id, t.topic, t.volume if t.volume else 0, 
+                        t.place.woeid, t.place.name)
+        if delete:
+            db.Trend.delete(t.id)
+            print u'Removed from db.'
+
+
+def _testTwoCities(freshPull=True):
+    """
+    Lookup trends for two cities.
+
+    Find topics which appear at least once, or are common or exclusive 
+    (one but not the other).
+
+    Set freshPull to false if you know you have the data in the db and
+    want to select it to work with it.
+    """
+    global api
+    # Specify country because there can be towns with same name in different countries.
+    country = 'South Africa'
+    townNames = ['Cape Town', 'Johannesburg']
+
+    # Orderby should be set explicitly here to avoid ambiguity error when place.id and country.id are used to compare, but order by is just on 'id'.
+    countryObj = db.Country.selectBy(name=country).orderBy('place.id')
+    countryID = countryObj.getOne().id
+
+    townObjList = []
+    for tName in townNames:
+        townObj = db.Town.select(builder.AND(db.Town.q.name == tName,
+                                             db.Town.q.countryID == countryID)
+                                ).orderBy('place.id')
+        townObjList.append(townObj.getOne())
+
+    # Now we have the town objects we can look up trends against their IDs.
+
+    if freshPull:
+        print 'INSERT INTO DB'
+        print 'ID Name WOEID'
+        for x in townObjList:
+            print x.id, x.name, x.woeid
+            insertTrendsForWoeid(x.woeid)
+            print
+        print
+
+    print 'DO STATS ON TOPICS'
+    # Timestamp - select from start of today.
+
+    # Retrieve the values we just added to the db. Normally the time gap would
+    # be larger - cronjob to insert the records on schedule and at another
+    # time in the day get records for that day or any date range.
+
+    woeidList = [x.woeid for x in townObjList]
+
+    # Select Place objects (and ids) for Places we are looking for.
+    subquery = db.Place.select(builder.IN(db.Place.q.woeid, 
+                                          woeidList))
+    print subquery
+
+    # Select trends matching any of the Places we are looking for.
+    todayTrends = db.Trend.select(builder.AND(
+        builder.IN(db.Trend.q.placeID, subquery),
+        db.Trend.q.timestamp >= datetime.date.today()
+        )
+    )
+
+    print todayTrends.count()
+
+    for t in todayTrends:
+        print t.topic
+    print
+
+    print 'DO STATS ON TOPICS - ALTERNATIVE'
+    # Instead of using IN as above and putting all trends together and then having to look up what regularly queries to get what place matches the place ID, this approach keeps the trends separated by place using a list (this could be list of dictionaries so it wouldn't have to be matched with the first list).
+    # Use for loop instead of a subquery.
+
+    todayTrendsAlt = []
+    for x in townObjList:
+        trendGroup = []
+        trendSelection = db.Trend.select(builder.AND(
+            db.Trend.q.timestamp >= datetime.date.today(),
+            db.Trend.q.placeID == x.id
+            )
+        )
+        for t in trendSelection:
+            trendGroup.append(t)
+        todayTrendsAlt.append(trendGroup)
+
+    for i, x in enumerate(todayTrendsAlt):
+        print townObjList[i].name.upper()
+        print len(x)
+        for y in x:
+            print y.topic
+        print
+
+
+    # Next step: use sets and Counter.
+
+
 if __name__ == '__main__':
-    _test()
+    # _testSearchTowns()
+    # _testSearchCountry()
+    # _testHighestWithDate()
+    _testTwoCities(freshPull=False)
