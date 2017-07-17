56514195d968e289830ff9f495fa875a4488fed2
Michael on Ubuntu <michael.currin@gmail.com>
2017-07-17 23:29:26 +0200

    Improved tests and ran them on a different machine.

--- tests/basic.sh
+++ tests/basic.sh
@@ -1,17 +1,23 @@
-#!/bin/env bash
+#!/bin/bash
 echo "Starting tests."
-cd ../app
-
+cd ..
 pwd
 echo
 
-# how to exit with error code like python exit(1)?
-#source ../virtualenv/bin/actvate || echo "ERROR Could not activate virtual env"; exit
-# Temporary solution.
-# exit? see example
 echo "Virtual env"
-source ~/.local/bin/twitterverse/bin/activate || echo "fail"
-echo "active"
+if [ -d "virtualenv" ]; then
+  echo "Found virtualenv folder or symlink"
+else
+  echo "Could not find virtualenv folder or symlink. Create it and install the packages in requirements.txt inside it."; exit 1
+fi;
+
+pwd
+# Todo - how to exit on failure?
+source virtualenv/bin/activate || echo "failed to activate"
+echo "activated"
+
+cd app
+pwd
 
 # Hide output but log as success. Errors are still shown.
 echo 'setupConf'
@@ -19,19 +25,25 @@ python lib/setupConf.py > /dev/null && echo '-> success'
 echo
 echo 'model'
 python models/model.py > /dev/null && echo '-> success'
+
 echo
-echo 'database'
-python lib/database.py > /dev/null && echo '-> success'
+echo 'Setup tables if they do not exist'
+python lib/database.py -c "initialise()" \
+   > /dev/null && echo '-> success'
 echo
+
 echo 'dbQueries'
 python lib/dbQueries.py > /dev/null && echo '-> success'
 echo
+
 echo 'textHandling'
 python lib/textHandling.py > /dev/null && echo '-> success'
 echo
-echo 'trends'
-python lib/trends.py > /dev/null && echo '-> success'
-echo
+
+# This test only becomes meaningful when it has something useful to test and runs in main.
+#echo 'trends'
+#python lib/trends.py > /dev/null && echo '-> success'
+#echo
 
 # echo 'TwitterAuth'
 # python lib/TwitterAuth.py > /dev/null && echo '-> success'
