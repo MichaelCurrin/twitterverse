719290b82ab08f074e067bdf03e139db96381c88
MichaelCurrin <michael.currin@gmail.com>
2017-07-28 19:38:32 +0200

    Moved database select tests out of app file.

--- app/lib/trends.py
+++ app/lib/trends.py
@@ -2,6 +2,8 @@
 """
 Retrieve Trend data for places from the Twitter API and insert into the database.
 """
+from sqlobject.sqlbuilder import LIKE, Select
+
 if __name__ == '__main__':
     # Allow imports of dirs in app, when executing this file directly.
     import os
@@ -11,8 +13,8 @@ from lib import database as db
 from lib import twitterAuth
 
 # This connection is done once and made available globally.
-globalApi = twitterAuth.getAPIConnection()
-
+##globalApi = twitterAuth.getAPIConnection()
+# Useful for inserting not when getting search for main or for import...
 
 def insertTrendsForWoeid(woeid, userApi=None, delete=False):
     """
@@ -50,3 +52,39 @@ def insertTrendsForWoeid(woeid, userApi=None, delete=False):
             print u'Removed from db.'
         else:
             print
+
+
+def search(searchStr='', orderByVol=False):
+    """
+    Search existing trends in the db for topics matching the input string.
+
+    Searches are case insensitive.
+
+    @param searchStr: word or phrase as a string for text to search in the
+        topic column of Trend table. Leave as default empty string to
+        not filter results. Multi-word searches are not possible except as
+        phrases.
+    """
+    orderBy = 'MaxVol DESC' if orderByVol else 'Trend.topic ASC'
+    query = """
+        SELECT Trend.topic, MAX(Trend.volume) AS MaxVol
+        FROM Trend
+        WHERE Trend.topic LIKE '%{0}%'
+        GROUP BY Trend.topic
+        ORDER BY {1}
+    """.format(searchStr, orderBy)
+   
+    res = db.conn.queryAll(query)
+
+    # note that volume can be added up, but any null values will not be counted.
+    print 'Max Volume | Topic'
+    for item in res:
+        # Making u'' causes errors for some reason for "Динамо"
+        print '{0:10,d} | {1}'.format(item[1] if item[1] else -1, item[0])
+
+if __name__ == '__main__':
+    search('', orderByVol=True)
+
+# Next step - volume. count distinct places. consider distinct over dates.
+# use new attributes in model.
+# Date range? timestamp as date col but then consider vol. Range limit?
