06ebd498b01c590f189cb823d6b48de0abf1aa98
Michael on Ubuntu <michael.currin@gmail.com>
2017-07-29 17:28:40 +0200

    Removed unnecessary use of app connection when calling lib/trends.py.

--- app/lib/trends.py
+++ app/lib/trends.py
@@ -10,9 +10,12 @@ To execute this file directly but still enable imports from app dir:
 from lib import database as db
 from lib import twitterAuth
 
-# This connection is done once and made available globally.
-globalApi = twitterAuth.getAPIConnection()
-# Useful for inserting not when getting search for main or for import...
+
+# Global object to be used as api connection. During execution of the insert
+# function, this can be setup once with default app then reused later,
+# to avoid time calling Twitter API. It can be left as null if not needed.
+appApi = None
+
 
 def insertTrendsForWoeid(woeid, userApi=None, delete=False):
     """
@@ -23,18 +26,28 @@ def insertTrendsForWoeid(woeid, userApi=None, delete=False):
     times which we don't need if we just use current time.
 
     @param woeid: Integer WOEID for a Place.
-    @param useApi: tweepy API connection object. Setting this here with a user-authorised object will mean the default app-authorised object at the global level will be ignored.
+    @param useApi: tweepy API connection object. Set this with a
+        user-authorised connection to skip the default behaviour of generating
+        and using an app-authorised connection.
     @param delete: Boolean, default False. If set to True, delete item after
         it is inserted into db. This is useful for testing.
     """
+    global appApi
+
     print 'Inserting trend data for', woeid
 
-    assert isinstance(woeid, int), 'Expected WOEID as type `int` but got type `{}`.'.format(type(woeid).__name__)
+    assert isinstance(woeid, int), ('Expected WOEID as type `int` but got '
+                                    'type `{}`.'.format(type(woeid).__name__))
 
     if userApi:
+        # Use user token.
         api = userApi
     else:
-        api = globalApi
+        # Use app token.
+        if not appApi:
+            # Set it if necessary and then reuse it next time.
+            appApi = twitterAuth.getAPIConnection()
+        api = appApi
     response = api.trends_place(woeid)[0]
     trends = response['trends']
 
@@ -80,9 +93,11 @@ def search(searchStr='', orderByVol=False):
         # Making u'' causes errors for some reason for "Динамо"
         print '{0:10,d} | {1}'.format(item[1] if item[1] else -1, item[0])
 
+
 if __name__ == '__main__':
     search('', orderByVol=True)
 
 # Next step - volume. count distinct places. consider distinct over dates.
 # use new attributes in model.
 # Date range? timestamp as date col but then consider vol. Range limit?
+# Where do search functions belong?
