a362c5e9bee7cb4fd601e640c90d7b08e58e4818
Michael on Ubuntu <michael.currin@gmail.com>
2017-08-16 23:09:22 +0200

    Improve topic stats report generator with flexible options and better docs.

--- app/utils/reporting/topicStatsReport.sh
+++ app/utils/reporting/topicStatsReport.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-
+###############################################################################
+#
+# Generate the topic stats report with a query to the database and send
+# the results out to a pre-configured CSV file for the current date.
+#
+# This script must be run with app as current working directory.
+#
 # Usage:
 # $ cd app
-# $ ./topicStatsReport.sh
-# $ ls ../../var/reporting
-# Now you can view the report.
+# $ ./utils/reporting/topicStatsReport.sh [pathToDB]
+#     where pathToDB is the configured db path. Defaults to 'var/db.sqlite'
+#     if not supplied.
+#
+# $ view var/reporting/
+# Now you can view the report, which starts with topic_report in the name
+# and has the current date. Any existing report on the day will be overwritten.
+#
+###############################################################################
 
-# # Assume this is run from dir of the script then change to app dir.
-cd ../..
 
-sqlite3 var/db.sqlite -csv -header \
-  < lib/query/sql/topicStats.sql \
-  > var/reporting/topic_report_$(date +"%y-%m-%d@%T").csv
+# Check for first argument otherwise use the default.
+if [ ! -z "$1" ]
+  then
+    dbName=$1
+  else
+    dbName=db.sqlite
+fi
+
+echo DB path: $dbName
+
+inPath=lib/query/sql/topicStats.sql
+echo Report: $inPath
+
+outPath=var/reporting/topic_report_$(date +"%y-%m-%d").csv
+
+sqlite3 $dbName -csv -header < $inPath > $outPath && echo Output: $outPath
