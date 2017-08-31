b172f790d848718ee6f4914f9216ac14a9087ee0
Michael on Ubuntu <michael.currin@gmail.com>
2017-08-31 23:32:21 +0200

    Move default trend job data to configured file. Add functionality to job manager file.

--- app/utils/jobManager.py
+++ app/utils/jobManager.py
@@ -1,45 +1,134 @@
 # -*- coding: utf-8 -*-
 """
+Job manager application file.
+
 Usage: $ python utils/jobManager.py
+       # OR
+       $ python
+       >>> from utils import jobManager as jm
+       >>> jm.insertCountry('United Kingdom')
+       >>> jm.getCounts()
 """
-import sys
 import os
+import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                             os.path.pardir)))
+                                                os.path.pardir)))
 
 from sqlobject.dberrors import DuplicateEntryError
 
+from etc.trendJobs import COUNTRIES, TOWN_PARENTS
 from lib import database as db
 
 
-def getItems():
-    print 'PlaceJob items'
-    print '=============='
+def getCounts():
+    """
+    Print stats for the PlaceJob table.
+    """
+    print 'PlaceJob stats'
+    print
     totalCount = db.PlaceJob.select().count()
     enabledCount = db.PlaceJob.selectBy(enabled=True).count()
     print 'enabled: {}'.format(enabledCount)
     print 'disabled: {}'.format(totalCount - enabledCount)
     print
 
-    for x in db.PlaceJob.selectBy(enabled=True):
-        print '{} {} {}'.format(x.placeID, x.place.name, x.created)
+
+def getRecords():
+    """
+    Print all records in the PlaceJob table.
+    """
+    print 'PlaceJob records'
+    print
+    template = '{0:>7} | {1:20} | {2:^10} | {3:^10} | {4:^10} | {5:^8}'
+    print template.format('Job ID', 'Place Name', 'Completed', 'Attempted', 'Created',
+                          'Enabled')
+
+    for x in db.PlaceJob.select():
+        data = (
+            x.id,
+            x.place.name,
+            str(x.lastCompleted.date()) if x.lastCompleted else '-' * 10,
+            str(x.lastAttempted.date()) if x.lastAttempted else '-' * 10,
+            str(x.created.date()),
+            x.enabled,
+        )
+        print template.format(*data)
+    print
 
 
-# Focus on mostly English speaking countries.
-countries = ['United States', 'South Africa', 'Ireland', 'United Kingdom',
-             'Canada', 'Australia', 'New Zealand', 'France', 'Italy',
-             'Germany', 'Spain']
 
-# Get all towns within these countries.
-getTownsFor = ['South Africa', 'Ireland']
+def clear():
+    """
+    Remove all records from PlaceJob table.
+    """
+    db.PlaceJob.clearTable()
 
-for c in countries:
-    o = db.Country.selectBy(name=c).getOne()
-    print o.id, o.countryCode, o.name,
+
+def insertCountry(name):
+    """
+    Add country to trend job list.
+
+    Expect country name and add it to the Place Job table, if it exists in
+    the Country table.
+
+    If the record exists, skip it. But if was disabled, enable it.
+    """
+    c = db.Country.selectBy(name=name).getOne()
+
+    output = (c.woeid, c.countryCode, c.name)
+
     try:
-        db.PlaceJob(placeID=o.id)
-        print '- inserted'
+        db.PlaceJob(placeID=c.id)
+        print '{0:10} | {1} | {2:15} - added'.format(*output)
     except DuplicateEntryError:
-        print '- skipped'
+        j = db.PlaceJob.selectBy(placeID=c.id).getOne()
+        if j.enabled:
+            print '{0:10} | {1} | {2:15} - found and already enbled '\
+                .format(*output)
+        else:
+            j.set(enabled=True)
+            print '{0:10} | {1} | {2:15} - found but made enabled'\
+                 .format(*output)
 
-getItems()
+
+def insertTownsOfCountry(name):
+    """
+    Add towns of a country to trend job list.
+
+    Expect country name and add its child towns to the Place Job table, if
+    the country exists in the Country table and if it has child towns.
+    """
+    c = db.Country.selectBy(name=name).getOne()
+    print '{0} {1}'.format(c.countryCode, c.name)
+
+    for t in c.hasTowns:
+        output = (t.woeid, t.name)
+        try:
+            db.PlaceJob(placeID=t.id)
+            print ' * {0:10} | {1:15} - added'.format(*output)
+        except DuplicateEntryError:
+            j = db.PlaceJob.selectBy(placeID=t.id).getOne()
+            if j.enabled:
+                print '{0:10} | {1:15} - found and already enbled '\
+                    .format(*output)
+            else:
+                j.set(enabled=True)
+                print '{0:10} | {1:15} - found but made enabled'\
+                     .format(*output)
+    print
+
+
+def defaultInserts():
+    print 'Inserting default values'
+    print '========================'
+    print
+    print 'Countries'
+    print '---------'
+    for c in COUNTRIES:
+        insertCountry(c)
+    print
+    print 'Towns'
+    print '-----'
+    for t in TOWN_PARENTS:
+        insertTownsOfCountry(t)
+    print
