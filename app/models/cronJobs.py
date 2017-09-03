e93ea080039749d6c200cd269828fa388b1c0c54
Michael on Ubuntu <michael.currin@gmail.com>
2017-09-03 15:19:26 +0200

    Update cronJob model with getStatus, adjusted ordering and better docs.

--- app/models/cronJobs.py
+++ app/models/cronJobs.py
@@ -21,10 +21,12 @@ from connection import conn
 
 class PlaceJob(so.SQLObject):
     """
-    Database table describing which places we want to get trends for on
-    a regular basis and when the last time the job was run.
+    Listing of places which we want to regularly get trend data for.
 
-    The WOEID of a place is used to look up trends at the location.
+    The WOEID of a place is used to look up trends at the location,
+    for records which have status set to enabled. An item can be marked
+    as disabled, so that it will be skipped by a procedure but kept in the
+    table so it can be enabled again easily.
 
     A place can appear only once in the table. Only towns or countries can be
     added to the job list, due to how Twitter API works - this is enforced
@@ -32,14 +34,17 @@ class PlaceJob(so.SQLObject):
 
     The table starts off empty and desired places can be added or removed
     depending on admin user's preferences.
-
-    An item can be marked as inactive, so that it will be skipped by a
-    procedure but kept in the table so it can be made active again easily.
     """
+
     class sqlmeta:
-        # Order as enabled (True) items first, then by jobs with oldest last
-        # completed timestamps first.
-        defaultOrder = 'enabled DESC, last_completed DESC'
+        # Order as enabled (True) items first, then by jobs with oldest
+        # (or null) last completed timestamps first and then by oldest
+        # (or null) last attempted timestamps. Therefore when running jobs, the
+        # ones which have not been completed before or for the longest time
+        # are given priority over ones which were recently completed.
+        # And any disabled jobs will be at the bottom when viewing a report.
+        defaultOrder = 'enabled DESC, last_completed ASC, last_attempted ASC'
+
     _connection = conn
 
     # Create a reference to Place table. Place IDs cannot be repeated in
@@ -85,3 +90,29 @@ class PlaceJob(so.SQLObject):
         Set the job to disabled.
         """
         self.enabled = False
+
+    def getStatus(self, asText=False):
+        """
+        Get the status from when the job was last run.
+
+        If last attempted time is None, then return None since we cannot
+        confirm success or failure.
+
+        Return True for success, if last completed time is not None and
+        is after last attempted time. Otherwise return False for failure.
+
+        @param asText: Default False. If True, return status as human-readable
+            string.
+
+        @return status: job status as OK (True) or failed (False)
+            or n/a (None). Returned as human-readable string if asText is True.
+        """
+        if self.lastAttempted:
+            if self.lastCompleted and self.lastCompleted > self.lastAttempted:
+                status = 'OK' if asText else True
+            else:
+                status = 'failed' if asText else False
+        else:
+            status = 'n/a' if asText else None
+
+        return status
