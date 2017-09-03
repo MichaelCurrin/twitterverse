cb39c9fa6aedaff05d0ec599036cd64d5a10bb31
Michael on Ubuntu <michael.currin@gmail.com>
2017-09-03 23:41:36 +0200

    Fix trendsPlaceJob.sh to print and log as intended.

--- tools/cron/trendsPlaceJob.sh
+++ tools/cron/trendsPlaceJob.sh
@@ -3,15 +3,24 @@
 # enabled places in the PlaceJob table.
 #
 # Usage:
+# This script must be run with twitterverse as the working directory.
 # In crontab, add a command change to the repo directory and then run this file from there.
 # That will ensure everything is done from the correct path.
 # e.g.
 # 0    0    *    *    *    cd ~/path/to/twitterverse && ./tools/cron/trendPlaceJob.sh
 
-# Run the place job schedule script and send stdout and stderr to a file
-# for the current day.
+echo 'Getting trends for Places in PlaceJob table.'
+
+source virtualenv/bin/activate || exit 1
 
 TODAY=$(date +'%Y-%m-%d')
+LOG_FILE="./app/var/log/cron/place_job_$TODAY.log"
 
-source virtualenv/bin/actvate | exit 1
-./app/utils/insert/runPlaceJobSchedule.py >> ./app/var/log/cron/place_job_$TODAY.log 2>&1
+echo "Writing to log file: $LOG_FILE"
+echo "Also printing to standard out..."
+echo
+
+# Run the place job schedule script. Print results to console (unbuffered
+# flag to get it in realtime) while also appending stdout to a log file named
+# with today's date.
+python -u app/utils/insert/runPlaceJobSchedule.py | tee -a $LOG_FILE
