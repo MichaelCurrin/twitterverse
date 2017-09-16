449c49901e4a8e202cb4cf08c7404747c7be54d9
MichaelCurrin <michael.currin@gmail.com>
2017-09-16 19:48:42 +0200

    Create models __init__.py file.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,10 @@
 """
 Initialisation file for models directory.
+
+Note that the model files cannot be be imported directly with
+`python -m models/{model}.py`, if they have been included here. Since
+this __init__ file will add the table names to the name space before the
+file is run, which causes a conflict.
 """
 # Create an _`_all__` list here, using values set in other application files.
 from .places import __all__ as placesModel
