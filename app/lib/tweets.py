a8fd9da4348f22ffaddbded4899cd6d08a9aed7a
MichaelCurrin <michael.currin@gmail.com>
2017-09-16 19:49:42 +0200

    Update batch tweet and profile fetching steps.

--- app/lib/tweets.py
+++ app/lib/tweets.py
@@ -13,8 +13,7 @@ These are the overall steps, which can be automated:
  3. Get tweet from the timeline of user and store in Tweets table, with link
     back to the Profile record. Repeat for watched users.
 """
-import os
-import sys
+import math
 
 from sqlobject.dberrors import DuplicateEntryError
 import tweepy
@@ -22,8 +21,7 @@ from tweepy.error import TweepError
 
 # While experimenting with tables not yet imported into db script,
 # use the tweets model file. The connection file also needs to be updated.
-# from lib import database as db
-from models import tweets as db
+from lib import database as db
 from lib.twitter import auth
 
 
@@ -58,24 +56,25 @@ def getProfile(APIConn, screenName=None, userID=None):
     return profile
 
 
-def insertOrUpdateProfile(profile):
+def insertOrUpdateProfile(fetchedProfile):
     """
     Insert record in Profile table or update existing record if it exists.
 
-    @param profile: tweepy profile object for one Twitter user.
+    @param fetchedProfile: single profile, as tweepy profile object fetched from
+        the Twitter API.
 
-    @return profileRec: SQLObject Profile table record.
+    @return profileRec: single profile, as SQLObject record in Profile table.
     """
     data = {
-        'guid': profile.id,
-        'screenName': profile.screen_name,
-        'name': profile.name,
-        'description': profile.description,
-        'location': profile.location,
-        'imageUrl': profile.profile_image_url_https,
-        'followersCount': profile.followers_count,
-        'statusesCount': profile.statuses_count,
-        'verified': profile.verified,
+        'guid':           fetchedProfile.id,
+        'screenName':     fetchedProfile.screen_name,
+        'name':           fetchedProfile.name,
+        'description':    fetchedProfile.description,
+        'location':       fetchedProfile.location,
+        'imageUrl':       fetchedProfile.profile_image_url_https,
+        'followersCount': fetchedProfile.followers_count,
+        'statusesCount':  fetchedProfile.statuses_count,
+        'verified':       fetchedProfile.verified,
     }
     try:
         # Attempt to insert new row, assuming GUID or screenName do not exist.
@@ -93,10 +92,12 @@ def insertOrUpdateProfileBatch(screenNames):
     """
     Get Twitter profile data from the Twitter API and store in the database.
 
+    Profile records are created, or updated they already exist.
+
     # TODO: Consider handling of printing of special chars.
 
     @param screenNames: list of user screen names as strings, to be fetched from
-        the Twitter API then added or updated in the local database.
+        the Twitter API.
 
     @return: None
     """
@@ -114,8 +115,10 @@ def insertOrUpdateProfileBatch(screenNames):
         else:
             try:
                 localProf = insertOrUpdateProfile(fetchedProf)
-                print u'Inserted/updated user: `{0}`'\
-                    .format(localProf.screenName)
+                # Represent log of followers count visually as repeated stars.
+                followersStars = '*' * int(math.log10(localProf.followersCount))
+                print u'Inserted/updated user: {0:20} - {1}'\
+                    .format(localProf.screenName, followersStars)
             except StandardError as e:
                 print u'Could not insert/update user: `{0}`. {1}. {2}'.format(
                     fetchedProf.screen_name, type(e).__name__, str(e)
@@ -128,8 +131,8 @@ def getTweets(APIConn, screenName=None, userID=None, tweetsPerPage=200,
     Get tweets of one profile from the Twitter API, for a specified user.
 
     Either screenName string or userID integer must be specified, but not both.
-    Calculate (tweetsPerPage)*(pageLimit) to get total number of tweets
-    requested from the API.
+    The result of (tweetsPerPage)*(pageLimit) indicates the total number
+    of tweets requested from the API on calling this function.
 
     @param APIConn: authenticated API connection object.
     @param screenName: Default None. The name of Twitter user to fetch, as
@@ -150,7 +153,8 @@ def getTweets(APIConn, screenName=None, userID=None, tweetsPerPage=200,
 
     assert screenName or userID, 'Expected either screenName (str) or userID'\
         '(int) to be set.'
-    assert not (screenName and userID), 'Cannot set both screenName and userID.'
+    assert not (screenName and userID), 'Cannot request both screenName and'\
+                                        ' userID.'
 
     params = {'count': tweetsPerPage}
     if screenName:
@@ -170,46 +174,113 @@ def getTweets(APIConn, screenName=None, userID=None, tweetsPerPage=200,
     return tweets
 
 
-def main(args):
+def insertOrUpdateTweet(fetchedTweet, profileID):
     """
-    Function to run when excecuting directly, fetching list of screen names,
-    either from arguments list or read from a specified text file.
+    Insert or update one record in the Tweet table.
 
-    @param args: command-line arguments as a list of strings. See
-        the usage help message below.
+    TODO: Write function to just update the fav and RT counts for an
+        existing tweet.
+
+    @param fetchedTweet: single tweet, as tweepy tweet object, as fetched from
+        the Twitter API.
+    @param profileID: The ID of the tweet's author, as an integer from
+        the Profile ID column in the local db. This is used to set
+        the Tweet object's foreign key.
+
+    @return tweetRec: single tweet, as SQLObject record in Tweet table.
+    """
+    data = {
+        'guid':          fetchedTweet.id,
+        'profileID':     profileID,
+        'message':       fetchedTweet.text,
+        'favoriteCount': fetchedTweet.favorite_count,
+        'retweetCount':  fetchedTweet.retweet_count
+    }
+    try:
+        # Attempt to insert new row, assuming GUID does not exit.
+        tweetRec = db.Tweet(**data)
+    except DuplicateEntryError:
+        tweetRec = db.Tweet.byGuid(data['guid'])
+        data.pop('guid')
+        # Replace values in existing record with those fetched from Twitter API.
+        tweetRec.set(**data)
+
+    return tweetRec
+
+
+def insertOrUpdateTweetBatch(profileRecs, tweetsPerProfile=200):
+    """
+    Get Twitter tweet data from the Twitter API for a batch of profiles
+    and store their tweets in the database.
+
+    TODO: Consider handling of printing of special chars.
+
+    TODO: Write function to look up list of tweet IDs and then insert or update
+        in local db. See tweepy's API.statuses_lookup docs. This can
+        be used to update tweets not updated recently.
+
+    @param profileRecs: list of Profile objects, to get create or update
+        tweets for. This could be a list from the Profile table which
+        has been filtered based on a job schedule, or Profiles which
+        match criteria such as high follower count.
+    @param tweetsPerProfile: Default 200. Count of tweets to get for each
+        profile, as an integer. If this is 200 or less, then page limit is
+        left at 1 and the count is reduced. If this is more than 200,
+        then count is left at 200 and page limit is adjusted to get a number
+        of tweets as the next multiple of 200. e.g. 550 tweets needs 2 pages
+        to get the first 400 tweets and a 3rd page to the additional 150 tweets,
+        which we simply as getting 200*2 = 600 tweets to keep the values
+        consistent on each query.
+
+        Note that even if 200 tweets are requested, the API sometimes returns
+        only 199.
+
+        The limit for a single request to the API is 200, therefore any
+        number up to 200 has the rate limit cost. It may be useful to set a
+        lower number if we want to get through all the users quickly,
+        as this takes fewer API queries and fewer db inserts or updates.
+        Also, consider that this may lead to deadtime, where the script is not
+        processing tweets in the database and just waiting for the next rate
+        limited window.
 
     @return: None
     """
-    if not args or set(args) & set(('-h', '--help')):
-        print 'Usage:'
-        print '$ python -m lib.tweets [-f|--file FILEPATH] [SCREENNAME, ...]'\
-            ' [-h|--help]'
-        print
-        print 'Options and arguments:'
-        print '--help    : Show this help output.'
-        print '--file    : Read in the following argument as path to text file,'
-        print '            instead of expecting list of screen names.'
-        print 'FILEPATH  : If used with --file flag, indicates the path to a'
-        print '            text file, which has one screen name per row and no'
-        print '            header or other data.'
-        print 'SCREENNAME: Supply a list of one or more Twitter screen names'
-        print '            to search for and add to the database. Cannot be'
-        print '            used with the --file flag.'
+    APIConn = auth.getAPIConnection()
+
+    if tweetsPerProfile <= 200:
+        tweetsPerPage = tweetsPerProfile
+        pageLimit = 1
     else:
-        if args[0] in ('-f', '--file') and len(args) == 2:
-            filename = args[1]
+        tweetsPerPage = 200
+        # If tweetsPerProfile is not a multiple of tweetsPerPage, then we
+        # have to add 1 page to the floor division calculation.
+        remainderPage = 1 if tweetsPerProfile % tweetsPerPage else 0
+        pageLimit = tweetsPerProfile / tweetsPerPage + remainderPage
 
-            assert os.access(filename, os.R_OK), 'Unable to read path `{0}`'\
-                                                 .format(filename)
-            # Read text file and split by newline characters.
-            with open(filename, 'rb') as reader:
-                screenNames = reader.read().splitlines()
+    for p in profileRecs:
+        try:
+            fetchedTweets = getTweets(APIConn, userID=p.guid,
+                                      tweetsPerPage=tweetsPerPage,
+                                      pageLimit=pageLimit)
+        except TweepError as e:
+            print u'Could not fetch tweets for user: `{0}`. {1}. {2}'.format(
+                p.screenName, type(e).__name__, str(e)
+            )
         else:
-            # We got no flags so we use the args list as screen names.
-            screenNames = args
+            print u'User: {0}'.format(p.screenName)
+            print 'Inserting/updating tweets...'
+            total = 0
+            for f in fetchedTweets:
+                try:
+                    localTweet = insertOrUpdateTweet(f, profileID=p.id)
 
-        insertOrUpdateProfileBatch(screenNames)
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+                except StandardError as e:
+                    print u'\nCould not insert/update tweet `{0}` for user'\
+                        ' `{1}`. {2}. {3}'.format(
+                            f.id, p.screenName, type(e).__name__, str(e)
+                        )
+                total += 1
+                # Print a total on every 10 processed.
+                if total % 10 == 0 or total == 200:
+                    print total
+            print 'Done - {0}'.format(total)
