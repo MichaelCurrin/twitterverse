ce2c613e43a05cebcb8c86dffb58462ebb01dd9f
MichaelCurrin <michael.currin@gmail.com>
2017-09-22 22:11:37 +0200

    Delete unused influencerProfiles.py file.

--- app/utils/insert/influencerProfiles.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Influencer Profiles application file.
-
-Iterate through a list Twitter influencer profile names, fetch their
-data from the Twitter API and insert into the local db.
-"""
