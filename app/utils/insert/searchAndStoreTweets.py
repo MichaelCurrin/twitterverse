02d84ee480fbcbd8eb886f387ebe72ee29ef7995
MichaelCurrin <michael.currin@gmail.com>
2017-10-05 23:11:44 +0200

    Update searchAndStoreTweets.py

--- app/utils/insert/searchAndStoreTweets.py
+++ app/utils/insert/searchAndStoreTweets.py
@@ -33,19 +33,28 @@ def searchAndStore(searchQuery, totalCount=200, persist=True):
     """
     Search Twitter API for tweets matching input search terms.
 
+    Only matches on tweets for users which had their language set to English
+    or undefined.
+
     @param searchQuery: search query as a string containing terms.
     @param totalCount: total count of tweets to attempt to get for the
         search query, as an integer. Defaults to 200, which is the max count
         of tweets received on a single page from the Twitter API.
     @param persist. Default True. If set to False, does not store data
         in the database and only prints to stdout.
+
+    @return processedCount: count of tweets processed, as an integer.
+        This will be a number up to the totalCount flag, but may less if
+        fewer tweets are available in the 10-day window, or some tweets
+        which were received were ignored because of language restriction
+        we apply.
     """
     assert API_CONN, ("Authenticate with Twitter API before doing"
                       " a search for tweets.")
 
     searchRes = search.fetchTweetsPaging(API_CONN, searchQuery=searchQuery,
                                          itemLimit=totalCount)
-
+    processedCount = 0
     for t in searchRes:
         if persist:
             # Add/update tweet author.
@@ -55,6 +64,9 @@ def searchAndStore(searchQuery, totalCount=200, persist=True):
         else:
             text = t.full_text.replace('\n', ' ').replace('\r', ' ')
             print u'@{0}: {1}'.format(t.author.screen_name, text)
+        processedCount += 1
+
+    return processedCount
 
 
 def main():
@@ -70,33 +82,39 @@ def main():
         formatter_class=argparse.RawTextHelpFormatter
     )
 
+    # Set value for terms list.
     searchQueryHelp = """\
-Search for tweets on Twitter API which match a query
-string containing one or more terms. See the Twitter API
-search documentation: https://dev.twitter.com/rest/public/search
+Search tweet messages on Twitter API which match a query
+string of one or more terms. See matching rules on
+Twitter API search documentation:
+    https://dev.twitter.com/rest/public/search
 
-Example on matching tweet message:
- * message contains all terms, in any order
+Any double quotes or hashtags must be escaped on
+the command-line.
+
+Examples:
+ * message contains all terms, in any order.
     - wordA
     - wordA wordB wordC wordD
- * message contains at least one of the terms
+ * message contains at least one of the terms.
     - wordA OR wordB
     - @handleA OR wordB OR wordC
- * message contains hashtag terms (must escape)
+ * message contains hashtag terms.
     - \\#abc
     - '#def OR xyz OR #ghi'
- * message contains exact phrases (must escape).
-    Note that quoted phrases must appear before
-    other terms.
+ * message contains exact phrases. Note that
+    quoted phrases must appear before other terms.
     - \\"My Quote\\"
     - '"My Quote" OR "Another quote" OR wordC'"""
     parser.add_argument('terms', metavar='TERM', nargs='+',
                         help=searchQueryHelp)
 
+    # Set value count. Allows -c N or -c=N syntax.
     parser.add_argument('-c', '--count', metavar='N', type=int, default=200,
                         help="Default 200. Max count of tweets to get for the"
                              " search query.")
 
+    # Set persist boolean value.
     parser.add_argument('--persist', dest='persist', action='store_true',
                         help="Store tweets and profiles in database."
                              " Defaults to on.")
@@ -113,7 +131,9 @@ Example on matching tweet message:
     # Use app auth for up to 450 search requests per window, rather than 180.
     API_CONN = auth.getAppOnlyConnection()
 
-    searchAndStore(args.terms, totalCount=args.count, persist=args.persist)
+    processedCount = searchAndStore(args.terms, totalCount=args.count,
+                                    persist=args.persist)
+    print 'Processed tweets: {0}'.format(processedCount)
 
 
 if __name__ == '__main__':
