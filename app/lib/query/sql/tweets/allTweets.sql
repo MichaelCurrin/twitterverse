84ca008bc1c7f54cc605f4b757110e6a8e83e17a
MichaelCurrin <michael.currin@gmail.com>
2017-10-20 22:20:50 +0200

    Create tweets sql dir with 4 queries.

---
+++ app/lib/query/sql/tweets/allTweets.sql
@@ -0,0 +1,23 @@
+/*
+    Select all tweets and match with their authors.
+
+    For replies, show the GUID of the target tweet and the screen name of the target profile.
+*/
+
+SELECT
+    Profile.guid AS author_guid,
+    '@' || Profile.screen_name AS author_screen_name,
+    Profile.image_url AS author_image_url,
+    Profile.followers_count AS author_follower_count,
+    Profile.location AS author_bio_location,
+    Tweet.guid AS tweet_guid,
+    DATE(Tweet.created_at) AS posted_date,
+    Tweet.favorite_count AS favs,
+    Tweet.retweet_count AS RTs,
+    Tweet.message,
+    Tweet.in_reply_to_tweet_guid AS replied_to_tweet_guid,
+    '@' || target_prof.screen_name AS replied_to_prof_screen_name,
+    target_prof.image_url AS replied_to_prof_image_url
+FROM Tweet
+INNER JOIN Profile ON Tweet.profile_id = Profile.id
+LEFT OUTER JOIN Profile AS target_prof ON target_prof.guid = Tweet.in_reply_to_profile_guid;
---
+++ app/lib/query/sql/tweets/basic/profiles.sql
@@ -0,0 +1,6 @@
+/*
+    Select all values in Profile table.
+*/
+
+SELECT guid, '@' || screen_name AS screen_name, name, followers_count, location, image_url
+FROM Profile;
