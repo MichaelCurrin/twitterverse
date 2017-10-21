36b257d967944d14b413c3505258c85c4f76edeb
MichaelCurrin <michael.currin@gmail.com>
2017-10-21 08:49:59 +0200

    Update allTweets.sql

--- app/lib/query/sql/tweets/allTweets.sql
+++ app/lib/query/sql/tweets/allTweets.sql
@@ -2,16 +2,22 @@
     Select all tweets and match with their authors.
 
     For replies, show the GUID of the target tweet and the screen name of the target profile.
+
+    GUIDs are formatted as strings so they read as text rather than integers when parsed as a CSV file.
+
+    DATETIME is used to remove microseconds from posted time.
+
 */
 
 SELECT
-    Profile.guid AS author_guid,
+    'ID_' || Profile.guid AS author_guid,
     '@' || Profile.screen_name AS author_screen_name,
     Profile.image_url AS author_image_url,
     Profile.followers_count AS author_follower_count,
     Profile.location AS author_bio_location,
-    Tweet.guid AS tweet_guid,
+    'ID_' || Tweet.guid AS tweet_guid,
     DATE(Tweet.created_at) AS posted_date,
+    DATETIME(Tweet.created_at) AS posted_datetime,
     Tweet.favorite_count AS favs,
     Tweet.retweet_count AS RTs,
     Tweet.message,
@@ -20,4 +26,5 @@ SELECT
     target_prof.image_url AS replied_to_prof_image_url
 FROM Tweet
 INNER JOIN Profile ON Tweet.profile_id = Profile.id
-LEFT OUTER JOIN Profile AS target_prof ON target_prof.guid = Tweet.in_reply_to_profile_guid;
+LEFT OUTER JOIN Profile AS target_prof ON target_prof.guid = Tweet.in_reply_to_profile_guid
+ORDER BY posted_datetime DESC;
