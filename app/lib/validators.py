1083c3ffad4d4a2f8b62f7018e183d47c3f13855
MichaelCurrin <michael.currin@gmail.com>
2018-01-28 23:15:27 +0200

    refactor: Update length validation on tweet and profile classes refactor: Update validators.py script

--- app/lib/validators.py
+++ app/lib/validators.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Validators lib application file.
+
+When using the to_python method on a validator or implementing a
+validator on a SQLObject column, a formencode Invalid error can be raised
+when necessary.
+
+Note on tweet message validation:
+    Twitter has changed the max at the end of 2017 from 140 to 280 characters
+    When a retweeted tweet is expanded it can exceed the limit, therefore
+    a validating on a max length does not work.
 """
 from formencode.validators import UnicodeString
 
 
-class TweetMessage(UnicodeString):
+class UnicodeValidator(UnicodeString):
     """
-    Twitter has changed the max at the end of 2017 from 140 to 280 characters
-    and this is mirrored here such that an Invalid error is raised for
-    exceeding the lengh. Note that Twitter applies the limit at creation
-    time on the *truncated* form of a tweet i.e. replace full URLs with
-    shortened 't.co' URLs, which only count as a small, fixed number of
-    characters.
+    Base for unicode string validation, with added support for evaluating
+    length correctly for special unicode characters, like accents and emjojis.
 
-    A tweet's URLs are expanded by default in the browser. This can be
-    recreated in the API using the 'extended' value for tweet_mode,
-    however this should be avoided as this means the tweet length exceeds
-    280 characters. Also the entire original URL is still not shown. If
-    this is required, then the model should be modified to store a tweet's
-    URL attribute.
+    Override `max` as an integer on the class or on an instance,
+    to enforce a maximum character length.
     """
-
-    max = 280
-
     def _validate_other(self, value, state):
         """
         Override the parent method so that validation is done on the unicode
@@ -35,4 +33,4 @@ class TweetMessage(UnicodeString):
         """
         if type(value) == str:
             value = value.decode('utf-8')
-        super(TweetMessage, self)._validate_other(value, state)
+        super(UnicodeValidator, self)._validate_other(value, state)
