27b0b6aaa3a0715cea2f27cce75b4d35fcb38365
MichaelCurrin <michael.currin@gmail.com>
2018-02-13 22:29:06 +0200

    docs: Update rates.py

--- app/lib/twitter/rates.py
+++ app/lib/twitter/rates.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Handle Twitter API rate limit error.
+
 This is an archive of code which is no longer needed but could be needed
 for an implementation to handle rate limiting in a different way.
 """
@@ -14,20 +16,30 @@ def limitHandled(cursor):
     (note that this does not work Streaming API.)
 
     Since cursors raise RateLimitErrors in their next() method, handling
-    them can be done by wrapping the cursor in an iterator.
+    them can be done by wrapping the cursor in an iterator, such that
+    an error is never raised outside the cursor.
 
-    This is ONLY needed if api object is setup to keep default
-    `wait_on_rate_limit_notify=False` which was a feature added to a later
-    version of tweepy.
+    Therefore this limitHandled function is is ONLY needed if an API
+    connection object is setup as `wait_on_rate_limit_notify=False`,
+    which was a flag added to a later version of tweepy.
 
     See tweepy docs and https://stackoverflow.com/questions/21308762/avoid-twitter-api-limitation-with-tweepy
 
-    @param: cursor: tweepy Cursor items list.
-        Usage:
-            for x in limitHandled(tweepy.Cursor(api.followers).items()):
-                print x
+    Note also that sleeping for 15 minutes is not efficient, as when you
+    exceed the limit for the 15 minute window, you could be a few seconds
+    from reaching the next window. It would be better to make a call
+    to the endpoint which returns rate limit data, or to just the rate limit
+    data returned on the main call (at least in the Twitter API and this
+    needs to be checked if its available in tweepy). Then the timestamp
+    of the when to wait until can be compared to current time and sleep until
+    then.
 
-    @return: None
+    @param: cursor: tweepy Cursor items list.
+        Example Usage:
+        >>> for x in limitHandled(tweepy.Cursor(api.followers).items()):
+        ...     print x
+
+    @return: cursor.next() in a generator expression.
     """
     while True:
         try:
