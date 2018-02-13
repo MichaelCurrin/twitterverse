69e25b877e3b7e70eb723d938d5f4800e7c9481b
MichaelCurrin <michael.currin@gmail.com>
2018-02-13 22:46:27 +0200

    docs: Update rates.py

--- app/lib/twitter/rates.py
+++ app/lib/twitter/rates.py
@@ -2,8 +2,19 @@
 """
 Handle Twitter API rate limit error.
 
-This is an archive of code which is no longer needed but could be needed
-for an implementation to handle rate limiting in a different way.
+The newer version of tweepy accepts the following the api object.
+- wait_on_rate_limit: Wait until next rate limit window is reached to continue
+    making queries. Default is True, so errors are prevented when iterating
+    over a cursor.
+- wait_on_rate_limit_notify: When waiting instead of raising an error,
+    show a warning message. See the tweepy binder.py script.
+        log.warning("Rate limit reached. Sleeping for: %d" % sleep_time)
+
+This rates script is an archive of code which was done based on tutorial in the
+documentation, but is longer needed but could be needed for an
+implementation to handle rate limiting in a different way if a hook is needed
+e.g. to log the warning to a different location or do another action
+instead of waiting. Using asyncio would be a good choice here.
 """
 import time
 
@@ -20,19 +31,15 @@ def limitHandled(cursor):
     an error is never raised outside the cursor.
 
     Therefore this limitHandled function is is ONLY needed if an API
-    connection object is setup as `wait_on_rate_limit_notify=False`,
+    connection object is setup as `wait_on_rate_limit=False`,
     which was a flag added to a later version of tweepy.
 
     See tweepy docs and https://stackoverflow.com/questions/21308762/avoid-twitter-api-limitation-with-tweepy
 
-    Note also that sleeping for 15 minutes is not efficient, as when you
+    TODO: Sleeping for 15 minutes is not efficient, as when you
     exceed the limit for the 15 minute window, you could be a few seconds
-    from reaching the next window. It would be better to make a call
-    to the endpoint which returns rate limit data, or to just the rate limit
-    data returned on the main call (at least in the Twitter API and this
-    needs to be checked if its available in tweepy). Then the timestamp
-    of the when to wait until can be compared to current time and sleep until
-    then.
+    from reaching the next window. Rather get the reset time and wait until
+    current time is that. See tweepy's binder.py script which does this.
 
     @param: cursor: tweepy Cursor items list.
         Example Usage:
