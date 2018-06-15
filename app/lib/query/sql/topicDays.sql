ec3db218e37c9bcc12f2254d74864d744ec7fbbb
MichaelCurrin <michael.currin@gmail.com>
2018-06-15 21:58:32 +0200

    style: Update topicDays.sql

--- app/lib/query/sql/topicDays.sql
+++ app/lib/query/sql/topicDays.sql
@@ -1,13 +1,19 @@
+/**
+ * Topics with how many days it was trending in at least one country, in the past 7 days.
+ */
 
--- Topics with how many days it was trending in at least one country, in the past 7 days.
-
-SELECT topic, COUNT(topic) AS days
-FROM  (
-    SELECT topic, DATE(Trend.timestamp) AS date, place_id
+SELECT
+    topic,
+    COUNT(topic) AS days
+FROM (
+    SELECT
+        topic,
+        DATE(Trend.timestamp) AS date,
+        place_id
     FROM Trend
     WHERE date >= DATE('now', '-7 days')
-    ) B
+) AS B
 INNER JOIN Country ON Country.id = B.place_id
 GROUP BY topic
-ORDER BY days ASC;
-
+ORDER BY days ASC
+;
