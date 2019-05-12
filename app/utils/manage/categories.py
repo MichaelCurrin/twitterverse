ea246c1eb5d3528181ed260ab840660dbdeb6c15
MichaelCurrin <michael.currin@gmail.com>
2019-05-12 21:26:07 +0200

    docs: Update categories.py

--- app/utils/manage/categories.py
+++ app/utils/manage/categories.py
@@ -311,11 +311,12 @@ def main():
     """
     parser = argparse.ArgumentParser(description="Category manager utility.")
 
-    subParser = parser.add_subparsers(help="Available subcommands")
+    subParser = parser.add_subparsers(help="Available subcommands. Use --help"
+                                           " after one for more info.")
 
     viewSubparser = subParser.add_parser(
         "view",
-        help="Print data to stdout"
+        help="Print data to stdout."
     )
     viewSubparser.add_argument(
         '-a', '--available',
