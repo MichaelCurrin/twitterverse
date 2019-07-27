b28b105b85eddaf496193daaeb14f380d6e32d6b
Michael Currin <michael.currin@gmail.com>
2019-07-27 16:13:12 +0200

    test: Add tests

--- app/tests/test_twitter_api.py
+++ app/tests/test_twitter_api.py
@@ -6,6 +6,13 @@ from lib.twitter_api import authentication
 
 class TestAuth(TestCase):
 
+    def test_generateAppToken(self):
+        auth = authentication._generateAppToken()
+
+    def test_getTweepyConnection(self):
+        auth = authentication._generateAppToken()
+        api = authentication._getTweepyConnection(auth)
+
     def test_getAPIConnection(self):
         api = authentication.getAPIConnection(userFlow=False)
 
