480bfa35bbcfb593ae31ed1f75e3249c71eeca15
Michael Currin <michael.currin@gmail.com>
2019-07-27 21:15:15 +0200

    feat: Update search_api.sh

--- app/tests/manual/search_api.sh
+++ app/tests/manual/search_api.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash -e
 # Regression test to Search API functionality which is done in util scripts.
-
-source venv/bin/activate
-cd app
+#
+# Run this from app dir with venv activated.
 
 echo 'Write to DB'
-./utils/insert/search_and_store_tweets.py -p 1 -q '#Monday'
+./utils/insert/search_and_store_tweets.py -p 2 -q '#Friday'
 echo
 
 echo 'Write to CSV'
-./utils/extract/search.py fetch -p 1 -q '#Monday'
+./utils/extract/search.py fetch -p 2 -q '#Friday'
