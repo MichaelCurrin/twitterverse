2fb1c14f9ad0b72f1f059d7e5e233b8001c2b60b
Michael Currin <michael.currin@gmail.com>
2019-09-21 16:50:50 +0200

    test: Update auth tests

--- app/tests/integration/test_twitter_api.py
+++ app/tests/integration/test_twitter_api.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Twitter API test module.
+
+Do requests to the Twitter API using configured credentials. NB. These require
+valid tokens for a Twitter dev account, plus a network connection.
 """
 from unittest import TestCase
 
+from lib.config import AppConf
 from lib.twitter_api import authentication
+
+conf = AppConf()
 
 
 class TestAuth(TestCase):
 
-    def test_generateAppToken(self):
+    def test_generateAppAccessToken(self):
         auth = authentication._generateAppAccessToken()
 
     def test_getTweepyConnection(self):
@@ -18,7 +24,7 @@ class TestAuth(TestCase):
 
     def test_getAPIConnection(self):
         """
-        Test App Access token.
+        Test that App Access token can be used to connect to Twitter API.
         """
         api = authentication.getAPIConnection(userFlow=False)
 
