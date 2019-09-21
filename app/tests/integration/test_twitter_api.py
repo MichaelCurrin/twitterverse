13a9dab2142ebf6dd403eefacdee3fa751f5c707
Michael Currin <michael.currin@gmail.com>
2019-09-21 16:45:35 +0200

    test: Correct method references

--- app/tests/integration/test_twitter_api.py
+++ app/tests/integration/test_twitter_api.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+Twitter API test module.
+"""
 from unittest import TestCase
 
 from lib.twitter_api import authentication
@@ -7,14 +10,20 @@ from lib.twitter_api import authentication
 class TestAuth(TestCase):
 
     def test_generateAppToken(self):
-        auth = authentication._generateAppToken()
+        auth = authentication._generateAppAccessToken()
 
     def test_getTweepyConnection(self):
-        auth = authentication._generateAppToken()
+        auth = authentication._generateAppAccessToken()
         api = authentication._getTweepyConnection(auth)
 
     def test_getAPIConnection(self):
+        """
+        Test App Access token.
+        """
         api = authentication.getAPIConnection(userFlow=False)
 
     def test_getAppOnlyConnection(self):
+        """
+        Test App-only token.
+        """
         api = authentication.getAppOnlyConnection()
