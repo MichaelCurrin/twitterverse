a558b09d687e3a64b4691d247ade160e6f808766
Michael Currin <michael.currin@gmail.com>
2019-12-02 16:40:29 +0200

    docs: Update help

--- app/utils/stream.py
+++ app/utils/stream.py
@@ -6,16 +6,6 @@ Test streaming API using command-line arguments list of input terms. Tweets
 are printed but not stored.
 
 See the docs directory for the AND / OR rules of stream searches.
-
-Transform items split to work with tweepy. Spaces on either side
-of commas are optional and have no effect.
-e.g.
-    $ python -m lib.twitter_api.streaming abc def,ABC DEF, xyz
-    => ['abc def', 'MNO QRS', 'xyz']
-    => which translates to
-        match ('abc' and 'def' in one tweet in any order)
-        or match ('MNO' and 'QRS' in one tweet in any order)
-        or match ('xyz')
 """
 import os
 import sys
@@ -31,9 +21,12 @@ def main(args):
     Main command-line entrypoint.
     """
     if not args or set(args) & {'-h', '--help'}:
-        print('Usage: {} [WORD, WORD, ...]'.format(__file__))
+        print('Usage: {} [PHRASE, PHRASE, ...]'.format(__file__))
         print()
-        print('For example, with arguments:')
+        print("Each phrase may have spaces and should be separated by")
+        print(" commas. They will then be ANDed together.")
+        print()
+        print('For example, with these arguments:')
         print('    abc def, MNO QRS,xyz')
         print('Then a stream search will be done for:')
         print('   ("abc" and "def") or ("MNO" and "QRS") or "xyz"')
