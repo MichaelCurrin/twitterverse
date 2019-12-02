14e637720d6c80ed88232130b00385ceb4d451da
Michael Currin <michael.currin@gmail.com>
2019-12-02 10:21:50 +0200

    chore: Create manual/__init__.py

---
+++ app/tests/manual/__init__.py
@@ -0,0 +1,7 @@
+"""
+Manual test module.
+
+Note that while `TEST_MODE` should be set an environment variable for the
+unit and integration tests, we want that off here so we can test against
+local config data.
+"""
