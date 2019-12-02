871275c305bc76fec49ebe4150f911e2be0c886f
Michael Currin <michael.currin@gmail.com>
2019-12-02 10:59:43 +0200

    feat: Create db_manager.py using database.py commands

--- app/lib/database.py
+++ app/lib/database.py
@@ -7,14 +7,8 @@ See docs for how to set up the database.
 Logging messages to a log file is not needed here as this is run directly
 as a command-line script. The detailed creation of places might be good to
 move to a log file with only summary level data printed to the console.
-
-Usage:
-    $ python -m lib.database --help
-
-TODO: Move the command-line aspects to utils directory script.
 """
 import os
-import sys
 
 from sqlobject import SQLObjectNotFound
 from sqlobject.dberrors import DuplicateEntryError
@@ -23,7 +17,6 @@ import models
 from etc import base_data
 from lib import locations
 from lib.config import AppConf
-from lib.db_query.schema import table_counts
 
 # Make model objects available on the database module.
 from models import (
@@ -235,7 +228,7 @@ def _checkDBexists():
     dbPath = conf.get('SQL', 'dbPath')
     status = os.path.exists(dbPath)
     print dbPath
-    print "Exists." if status else "Not created yet."
+    print "DB file exists." if status else "DB file not created yet."
     print
 
     return status
@@ -275,75 +268,3 @@ def _populate(maxTowns=None):
     print '-> Added fixtures data.\n'
 
     return maxTowns
-
-
-HELP_MSG = """\
-Usage:
-$ python -m lib.database [-p] [-s] [-d] [-c] [-P] [-h]
-
-Options and arguments:
--h --help    : Show help and exit.
--p --path    : Show path to configured db file.
--s --summary : Show summary of tables and records in db.
--d --drop    : Drop all tables.
--c --create  : Create all tables in models, but do not drop or alter
-               existing tables or modify their data. Then insert base data
-               for Campaign and Category labels (see config file), so they
-               can be assigned as labelling process within utilities.
-               Even the Campaign or Category tables existed already, base
-               records are still inserted. If a base record exists then its
-               creation is skipped.
--P --populate: Populate tables with default location data and relationships.
-               ONLY if used without other flags, accepts an optional
-               integer as max number of towns to create from fixtures data.
-               This is useful during development to save time, if only a few
-               or no towns are needed.
-
-Note:
-  Flags can be combined.
-  e.g. $ python -m lib.database -p -d -c -P -s
-  Actions will always be performed with the following priority from
-  first to last: drop -> create -> populate.
-"""
-
-
-def main(args):
-    """
-    Run functions using command-line arguments.
-    """
-    if len(args) == 0 or set(args) & {'-h', '--help'}:
-        print HELP_MSG
-
-        return HELP_MSG
-    else:
-        if set(args) & {'-p', '--path'}:
-            _checkDBexists()
-
-        if set(args) & {'-d', '--drop'}:
-            confirm_drop = raw_input(
-                "Are you sure you want to DROP all tables? [Y/N] /> ")
-            if confirm_drop.strip().lower() in ('y', 'yes'):
-                _dropTables()
-            else:
-                print 'Cancelled dropping tables. Exiting.'
-
-                return None
-
-        if set(args) & {'-c', '--create'}:
-            _createTables()
-            _baseLabels()
-
-        if set(args) & {'-P', '--populate'}:
-            if len(args) == 2 and args[1].isdigit():
-                limit = int(args[1])
-            else:
-                limit = None
-            _populate(limit)
-
-        if set(args) & {'-s', '--summary'}:
-            print 'Getting table summary...'
-            table_counts.showTableCounts()
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
