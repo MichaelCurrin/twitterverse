b87245187976caf0d918b12d1c19fb3cfdbb3685
Michael Currin <michael.currin@gmail.com>
2019-12-04 13:47:14 +0200

    test: Update search_api.sh

--- app/tests/manual/search_api.sh
+++ app/tests/manual/search_api.sh
@@ -3,24 +3,30 @@
 #
 # Run after after DB has been setup and with venv active.
 # For now, continue to use main DB and main credentials, rather than test DB.
-echo "Not stored"
+#
+# Use an arbitrary search term which will have volume. And just get a page
+# or two.
+
+echo "Using query"
 echo
-echo 'Write to DB script'
-./utils/insert/search_and_store_tweets.py -p 2 -q '#BlackFriday'
-echo
-
-echo 'Write to CSV script'
-./utils/extract/search.py fetch -p 2 -q '#BlackFriday'
-echo
-
-
-echo "Stored"
-# Requires this named campaign to exist in the DB.
-
-echo 'Write to DB script'
-./utils/insert/search_and_store_tweets.py -p 2 -c '#BlackFriday'
+echo 'Use write to DB script'
+./utils/insert/search_and_store_tweets.py -p 2 -q 'hello'
 echo
 
-echo 'Write to CSV script'
-./utils/extract/search.py fetch -p 2 -c '#BlackFriday'
+echo 'Use write to CSV script'
+./utils/extract/search.py fetch -p 2 -q 'hello'
+echo
+
+echo "Setup test campaign if it does not exist"
+utils/manage/campaigns.py -c '_TEST_SEARCH' -q 'hello'
+echo
+
+echo "Using campaign"
+
+echo 'Use write to DB script'
+./utils/insert/search_and_store_tweets.py -p 2 -c '_TEST_SEARCH'
+echo
+
+echo 'Use write to CSV script'
+./utils/extract/search.py fetch -p 2 -c '_TEST_SEARCH'
 echo
