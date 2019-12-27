18955765a256a65c592cde57ea49cf39a4e37792
Michael Currin <michael.currin@gmail.com>
2019-12-27 23:28:53 +0200

    docs: Add note

--- app/tests/manual/search_api.sh
+++ app/tests/manual/search_api.sh
@@ -7,7 +7,12 @@
 # Use an arbitrary search term which will have volume. And just get a page
 # or two.
 
+# TODO: Find a way to run against test DB but with actual API credentials.
+# export TEST_MODE=1
+
 echo "Using query"
+# Note that tweets will be added agains the _SEARCH_QUERY base campaign
+# but note against a specific campaign.
 echo
 echo 'Use write to DB script'
 ./utils/insert/search_and_store_tweets.py -p 2 -q 'hello'
@@ -18,7 +23,7 @@ echo 'Use write to CSV script'
 echo
 
 echo "Setup test campaign if it does not exist"
-utils/manage/campaigns.py -c '_TEST_SEARCH' -q 'hello'
+./utils/manage/campaigns.py -c '_TEST_SEARCH' -q 'hello'
 echo
 
 echo "Using campaign"
