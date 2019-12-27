f0f1144015932ab4624564457c2053c5b8d506c5
Michael Currin <michael.currin@gmail.com>
2019-12-27 23:28:45 +0200

    docs: Fix grammar

--- app/tests/unit/__init__.py
+++ app/tests/unit/__init__.py
@@ -5,6 +5,6 @@ from __future__ import absolute_import
 import os
 
 # Set global flag which is used to ensure test config values are used. This
-# must set here since the connection.py module is used indirectly by other
+# must be set here since the connection.py module is used indirectly by other
 # scripts so it is hard to pass a test flag to the config init.
 os.environ['TEST_MODE'] = "1"
