91dca4de7c87a563e3d35df95da4e2e91b701e2d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-25 21:07:29 +0200

    Update worldTrends.sql

--- app/lib/db_query/sql/trends/worldTrends.sql
+++ app/lib/db_query/sql/trends/worldTrends.sql
@@ -26,11 +26,14 @@ FROM (
         FROM Trend
         INNER JOIN Place ON Place.id = Trend.place_id
         WHERE Place.woeid = 1
-        -- TODO: Check if this is needed. See link to min timestamp and apply throughout this directory.
+        -- TODO: Check if this is needed. See link to min timestamp and apply
+        -- throughout this directory.
         ORDER BY Trend.timestamp ASC
     ) AS GlobalTrends
     WHERE date > DATE('NOW', '-2 DAYS')
     GROUP BY date, topic
 )
-ORDER BY volume, date DESC
+ORDER BY
+    volume ASC,
+    date DESC
 ;
