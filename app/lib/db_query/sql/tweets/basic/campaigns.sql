d3cf1c27690412854a0cc7eb1878091cf54788c4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-02-27 19:10:13 +0200

    style: update SQL

--- app/lib/db_query/sql/tweets/basic/campaigns.sql
+++ app/lib/db_query/sql/tweets/basic/campaigns.sql
@@ -1,10 +1,11 @@
 /*
-    Campaigns summary. Includes campaigns with no tweets.
-*/
-
+ Campaigns summary. Includes campaigns with no tweets.
+ */
 SELECT
-    Campaign.name,
-    COUNT(*) AS tweets
-FROM Campaign
-LEFT OUTER JOIN tweet_campaign ON tweet_campaign.campaign_id = Campaign.id
-GROUP BY name;
+  Campaign.name,
+  COUNT(*) AS tweets
+FROM
+  Campaign
+  LEFT OUTER JOIN tweet_campaign ON tweet_campaign.campaign_id = Campaign.id
+GROUP BY
+  name;
--- app/lib/db_query/sql/tweets/basic/profiles.sql
+++ app/lib/db_query/sql/tweets/basic/profiles.sql
@@ -1,12 +1,12 @@
 /*
-    Select all values in Profile table.
-*/
-
+ Select all values in Profile table.
+ */
 SELECT
-    guid,
-    '@' || screen_name AS screen_name,
-    name,
-    followers_count,
-    location,
-    image_url
-FROM Profile;
+  guid,
+  '@' || screen_name AS screen_name,
+  name,
+  followers_count,
+  location,
+  image_url
+FROM
+  Profile;
