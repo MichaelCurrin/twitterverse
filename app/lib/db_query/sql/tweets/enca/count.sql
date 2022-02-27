f23a52e209786818c56e120d1701797cb9b4eaaf
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-02-27 19:10:26 +0200

    chore: delete enca

--- app/lib/db_query/sql/tweets/enca/count.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    COUNT(*)
-FROM tweet
-INNER JOIN tweet_campaign ON tweet.id = tweet_campaign.tweet_id
-INNER JOIN campaign ON campaign.id = tweet_campaign.campaign_id
-WHERE campaign.name = 'ENCA';
--- app/lib/db_query/sql/tweets/enca/dates.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    DATE(Tweet.created_at) AS posted_date,
-    COUNT(*)
-FROM tweet
-INNER JOIN tweet_campaign ON tweet.id = tweet_campaign.tweet_id
-INNER JOIN campaign ON campaign.id = tweet_campaign.campaign_id
-WHERE campaign.name = 'ENCA'
-GROUP BY posted_date;
